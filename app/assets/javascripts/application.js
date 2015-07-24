2c3e00729dad5005554b0cc8829e54f4a7dfb29e
gavinhhy <821222837@qq.com>
2015-07-24 19:52:37 +0800

    初步完成发布博客的功能，待加上验证功能

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,11 +2,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
-//= require ckeditor/init
 //= require jquery.anystretch.min
 //= require move-top.js
 //= require easing.js
 //= require circles.js
 //= require bootstrap.min
 //= require npm.js
+//= require ckeditor/init
 //= require_tree .
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,51 @@
 class PostsController < ApplicationController
-    layout  'posts_index'
+  layout 'posts_index'
+
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "The article has been successfully created."
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @post= Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:title,:content)
+  end
+
+
 end
